dc1ff1cd8669f9110bd12db73eb73e7be9bb81de
uDaddyHenrique <beibedobeibe678@gmail.com>
2021-11-09 22:49:16 -0300

    Version 1.0

---
+++ src/main/java/me/henrique/itemapi/ItemAPI.java
@@ -0,0 +1,41 @@
+package me.henrique.itemapi;
+
+import me.henrique.itemapi.itembuilder.ItemBuilder;
+import me.henrique.itemapi.skullbuilder.SkullBuilder;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ItemAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("item").setExecutor(this);
+    }
+
+    /*
+    * Demonstração do ItemBuilder
+    * E SkullBuilder
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player){
+            Player player = (Player) sender;
+            if(args.length == 0){
+                //GIVANDO UMA CABEÇA PERSONALIZADA
+                ItemStack skull = new SkullBuilder("http://textures.minecraft.net/texture/e2b35bda5ebdf135f4e71ce49726fbec5739f0adedf01c519e2aea7f51951ea2", true).setDisplayName("&aCabeça Personalizada").setLore("§aLore 1", "§bLore 2.").toItemStack();
+                player.getInventory().setItem(1, skull);
+                //GIVANDO UM ITEM UTILIZANDO A CLASS ItemBuilder
+                ItemStack itemBuilder = new ItemBuilder(Material.DIAMOND_SWORD).setDisplayName("§aEspada Legal").setLore("§aLore 1.", "§bLore 2.").setEnchant(Enchantment.DAMAGE_ALL, 2, true).setUnbreaking(true).toItemStack();
+                player.getInventory().setItem(2, itemBuilder);
+                player.sendMessage("§aItem givado com sucesso.");
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/me/henrique/itemapi/itembuilder/ItemBuilder.java
@@ -0,0 +1,78 @@
+package me.henrique.itemapi.itembuilder;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+/**
+ * @Author: DaddyHenrique
+ * @Repository: https://github.com/uDaddyHenrique/ItemAPI
+ */
+
+/*
+* Como usar?
+* para criar um item basta utilizar
+* ItemStack item = new ItemBuilder(Material.DIAMOND_SWORD).toItemStack(); OBS: O Material pode ser qualquer um, para ver os disponíveis acesse https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/Material.html
+* como posso adicionar um nome ao item?
+* ItemStack item = new ItemBuilder(Material.DIAMOND_SWORD).setDisplayName("§aNome do item").toItemStack();
+* como posso adicionar uma lore ao item?
+* ItemStack item = new ItemBuilder(Material.DIAMOND_SWORD).setLore("§aLore 1", "§bLore 2").toItemStack();
+* como posso adicionar um encantamento ao item?
+* ItemStack item = new ItemBuilder(Material.DIAMOND_SWORD).setEnchant(Enchantment.DAMAGE_ALL, 2, true).toItemStack(); Obs: LEMBRANDO QUE PODE UTILIZAR QUALQUER ENCANTAMENTO BASTA VER OS DISPONÍVEIS EM https://helpch.at/docs/1.8.8/index.html?org/bukkit/enchantments/Enchantment.html
+* como posso deixar o item inquebravél?
+* ItemStack item = new ItemBuilder(Material.DIAMOND_SWORD).setUnbreaking(true).toItemStack();
+ */
+
+public class ItemBuilder {
+
+    private static ItemStack itemStack;
+
+    public ItemBuilder(Material material){
+        itemStack = new ItemStack(material);
+    }
+
+    public ItemBuilder(ItemStack item){
+        itemStack = new ItemStack(item);
+    }
+
+    public ItemBuilder(Material material, int amount, short data){
+        itemStack = new ItemStack(material, amount, data);
+    }
+
+    public ItemBuilder setDisplayName(String name){
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(name);
+        itemStack.setItemMeta(meta);
+        return this;
+    }
+
+    public ItemBuilder setLore(String... lore){
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setLore(Arrays.asList(lore));
+        itemStack.setItemMeta(meta);
+        return this;
+    }
+
+    public ItemBuilder setEnchant(Enchantment enchant, int level, boolean active){
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.addEnchant(enchant, level, active);
+        itemStack.setItemMeta(meta);
+        return this;
+    }
+
+    public ItemBuilder setUnbreaking(boolean unbreaking){
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.spigot().setUnbreakable(unbreaking);
+        itemStack.setItemMeta(meta);
+        return this;
+    }
+
+    public ItemStack toItemStack(){
+        ItemMeta meta = itemStack.getItemMeta();
+        itemStack.setItemMeta(meta);
+        return itemStack;
+    }
+}
